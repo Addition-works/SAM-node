50188df4d4b3d4c687fd84efacbe3b8ab063cff5
Rick Barber <rick@addition.works>
2024-09-09 15:14:41 -0500

    update

--- samnode.py
+++ samnode.py
@@ -29,6 +29,15 @@ print("Imported SAM node")
 print("Folder paths models: ", folder_paths.models_dir)
 
 
+def tensor2im(var):
+    var = var.cpu().detach().transpose(0, 2).transpose(0, 1).numpy()
+    var = ((var + 1) / 2)
+    var[var < 0] = 0
+    var[var > 1] = 1
+    var = var * 255
+    return var.astype('uint8')
+
+
 class SamNode:
     @classmethod
     def INPUT_TYPES(cls):
