f76e43913af161a4ce3908687de4f42d307161e5
Rick Barber <rick@addition.works>
2024-09-09 19:25:41 -0500

    update

--- samnode.py
+++ samnode.py
@@ -7,7 +7,8 @@ import folder_paths
 print('folder_paths.base_dir: ', folder_paths.base_path)
 sampath = os.path.join(folder_paths.base_path, 'custom_nodes/SAM-node/SAM')
 nodepath = os.path.join(folder_paths.base_path, 'custom_nodes/SAM-node')
-print(sampath)
+print('sampath: ', sampath)
+print('nodepath: ', nodepath)
 sys.path.append(os.path.abspath('/workspace/ComfyUI/custom_nodes/SAM-node/SAM'))  # Path to SAM
 sys.path.append(os.path.abspath('/workspace/ComfyUI/custom_nodes/SAM-node'))     # Path to custom node root
 
